4e7bfeb2ed5685d13b2f0ee5a9b428749c6c9fc3
dantegamesalex <dantegamesalex@gmail.com>
2021-05-10 19:30:52 +0300

    Добавлен медиатор для основного окна

---
+++ PasswordApp/Services/MVVMCore/MediatorMainWindow.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordApp.Services.MVVMCore
+{
+    public static class MediatorMainWindow
+    {
+        public delegate void ViewModel();
+
+        private static readonly IDictionary<string, List<ViewModel>> dictionary =
+           new Dictionary<string, List<ViewModel>>();
+
+        public static void Subscribe(string token, ViewModel callback)
+        {
+            if (!dictionary.ContainsKey(token))
+            {
+                var list = new List<ViewModel>
+                {
+                    callback
+                };
+                dictionary.Add(token, list);
+            }
+            else
+            {
+                bool found = false;
+                foreach (var item in dictionary[token])
+                    if (item.Method.ToString() == callback.Method.ToString())
+                        found = true;
+                if (!found)
+                    dictionary[token].Add(callback);
+            }
+        }
+
+        public static void Unsubscribe(string token, ViewModel callback)
+        {
+            if (dictionary.ContainsKey(token))
+                dictionary[token].Remove(callback);
+        }
+
+        public static void Notify(string token)
+        {
+            if (dictionary.ContainsKey(token))
+                foreach (var callback in dictionary[token])
+                    callback();
+        }
+    }
+}
