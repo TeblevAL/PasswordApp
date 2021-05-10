6c0e81e65ced480e811bd129cf54df310a28f299
dantegamesalex <dantegamesalex@gmail.com>
2021-05-10 21:08:36 +0300

    Описаны комманды NavigationViewModel

--- PasswordApp/ViewModels/NavigationViewModel.cs
+++ PasswordApp/ViewModels/NavigationViewModel.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.Win32;
+using PasswordApp.Models;
+using PasswordApp.Services.Cryptography;
 using PasswordApp.Services.MVVMCore;
+using PasswordApp.Views.Dialogs;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -45,25 +48,73 @@ namespace PasswordApp.ViewModels
 
         private void Home()
         {
-           
+            SelectedViewModel = new HomeViewModel();
         }
 
         private void Ecsport()
         {
-           
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "PA files(*.pa)|*.pa";
+
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                KeyDialog keyDialog = new KeyDialog("Введите ключ для шифрования файла");
+                if (keyDialog.ShowDialog() == true)
+                {
+                    List<Account> ecsportList = new List<Account>();
+                    foreach (var account in ModelsController.Accounts)
+                    {
+                        ecsportList.Add(new Account { SiteName = account.SiteName, Login = account.Login, Password = account.Password, Url = account.Url });
+                    }
+                    CryptoController.Current.Algorithm.EncryptFile(saveFileDialog.FileName, ecsportList, keyDialog.Key);
+                }
+            }
         }
 
         private void Import()
         {
-           
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "PA files(*.pa)|*.pa";
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                KeyDialog keyDialog = new KeyDialog("Введите ключ для расшифровки файла");
+                if (keyDialog.ShowDialog() == true)
+                {
+                    List<Account> importList = CryptoController.Current.Algorithm.DecryptFile<Account>(openFileDialog.FileName, keyDialog.Key);
+                    Category category = ModelsController.Categories.Where(ob => ob.Name == "[Импортированные]").FirstOrDefault();
+                    if (category != null)
+                    {
+                        foreach (var account in importList)
+                        {
+                            account.IdCategory = category.Id;
+                            ModelsController.AddAccount(account);
+                        }
+
+                    }
+                    else
+                    {
+                        ModelsController.AddCategory(new Category { Name = "[Импортированные]" });
+                        Category newCategory = ModelsController.Categories.Where(ob => ob.Name == "[Импортированные]").FirstOrDefault();
+                        foreach (var account in importList)
+                        {
+                            account.IdCategory = newCategory.Id;
+                            ModelsController.AddAccount(account);
+                        }
+                    }
+                }
+            }
         }
 
         private void Settings()
         {
+            SelectedViewModel = new SettingsViewModel();
         }
 
         private void Exit()
         {
+            ModelsController.EncryptPasswords();
+            CryptoController.DeleteTrueKey();
             MediatorMainWindow.Notify("LoadLogInView");
         }
         #endregion
@@ -74,7 +125,7 @@ namespace PasswordApp.ViewModels
         {
 
             HomeCommand = new RelayCommand(obj => Home());
-            EcsportCommand = new RelayCommand(obj => Ecsport());
+            EcsportCommand = new RelayCommand(obj => Ecsport(),obj => ModelsController.Accounts.Count > 0);
             ImportCommand = new RelayCommand(obj => Import());
             SettingsCommand = new RelayCommand(obj => Settings());
             ExitCommand = new RelayCommand(obj => Exit());
