46cebd0abef30fd8b441bf9cb44b607691ccab41
dantegamesalex <dantegamesalex@gmail.com>
2021-05-10 20:03:43 +0300

    Добавленн атрибут валидации не позволяющий вводить кирилицу

---
+++ PasswordApp/Services/DataAnnotations/LatinStringAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace PasswordApp.Services.DataAnnotations
+{
+    public class LatinStringAttribute : ValidationAttribute
+    {
+        string reg = @"^[A-Za-z0-9\W]+$";
+
+        public override bool IsValid(object value)
+        {
+            string valueString = value as string;
+            if (!string.IsNullOrEmpty(valueString))
+            {
+                return Regex.Match(valueString, reg).Success;
+            }
+            return true;
+        }
+    }
+}
